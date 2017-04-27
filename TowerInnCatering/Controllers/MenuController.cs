25fea823df8b586cda6fe998e5c426b664d7592f
Auston Lopez <auston.lopez.83@gmail.com>
2017-04-26 20:33:05 -0500

    Making of the Menu

---
+++ TowerInnCatering/Controllers/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TowerInnCatering.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TowerInnCatering.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Menu()
+        {
+            MenuItem[] array =
+            {
+                new MenuItem {Name="Chicken", Price=10},
+                new MenuItem {Name="Beef", Price=10},
+                new MenuItem {Name="Fish", Price=15}
+
+            };
+            return View(array);
+        }
+    }
+}
---
+++ TowerInnCatering/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TowerInnCatering.Models
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Catagory { get; set; }
+        public int Price { get; set; }
+        public bool InVisable { get; set; }
+
+    }
+}

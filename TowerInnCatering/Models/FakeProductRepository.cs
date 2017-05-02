5124bb75964a3f23d85b4cb118d26da1b2081d1c
Auston Lopez <Auston Lopez>
2017-05-02 09:12:46 -0500

    working on caigorizing the list view

--- TowerInnCatering/Models/FakeProductRepository.cs
+++ TowerInnCatering/Models/FakeProductRepository.cs
@@ -5,8 +5,13 @@ namespace TowerInnCatering.Models
     {
         public IEnumerable<Product> Products => new List<Product>
         {
-            new Product { Name = "Chicken",Price = 10},
-            new Product { Name = "Beef", Price = 14},
+             //chicken
+            new Product { Name = "Dijon Ritz Chicken", Category= "Chicken",Price = 10M},
+            new Product { Name = "Chicken Pacatta", Category = "Chicken", Price = 10M},
+            //beef
+            new Product { Name = "Beef Borgonion", Category="Beef", Price = 14M},
+            new Product { Name = "Beef and Onions", Category="Beef", Price = 13M},
+            //fish
         };
     }
 }
--- TowerInnCatering/Models/Product.cs
+++ TowerInnCatering/Models/Product.cs
@@ -4,6 +4,7 @@ namespace TowerInnCatering.Models
     public class Product
     {
         public string Name { get; set; }
-        public int Price { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
     }
 }

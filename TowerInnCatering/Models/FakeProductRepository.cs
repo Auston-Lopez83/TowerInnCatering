b3de616e585b714f67379cc41d78c0381e462cf4
Auston Lopez <Auston Lopez>
2017-05-02 19:40:52 -0500

    Getting the fakePRepo ready

--- TowerInnCatering/Models/FakeProductRepository.cs
+++ TowerInnCatering/Models/FakeProductRepository.cs
@@ -5,12 +5,13 @@ namespace TowerInnCatering.Models
     {
         public List<Product> Products => new List<Product>
         {
-            //chicken & Pasta
+            //
             new Product { Name = "Chicken and Pasta", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = "~/images/banner1.jpg"},
             new Product { Name = "The Tower Inn’s Grand Buffet", Description = "Roast Beef, Ritz Dijon Chicken, Sun-dried Tomato Penne, Parmesan Roasted Potatoes, Vegetable Medley, and Fresh Baked Bread.", Price = 19m, Image = "~/images/banner2.jpg"},
             new Product { Name = "The Pasta Buffet", Description = "Ritz Dijon Chicken, BBQ Chicken, Chicken Piccata, your choice of two pastas, and fresh baked bread.", Price = 10M, Image = "~/images/banner3.jpg"},
             new Product { Name = "The Pasta Buffet", Description = "Your choice of two pastas, vegetable, & breadsticks.", Price = 9M,Image =""},
             new Product { Name = "Pizza and Pasta Buffet", Description = "Your choice of gourmet pizzas, pastas, garlic breadsticks, and cinnamon twists.", Price = 9M,Image =""},
+            new Product { Name = "Fish and Chips", Description = "Fried Fish with Friz.", Price = 18.45M, Image = "~/images/banner1.jpg"},
 
         };
         //public List<Fish> Fishs => new List<Fish>
